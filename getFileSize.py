66eafcccfef6f569775d6fd33585eddd5e5626da
Matt <ventis@gmail.com>
2016-11-06 13:30:44 -0500

    Update getFileSize.py

--- getFileSize.py
+++ getFileSize.py
@@ -2,6 +2,6 @@ import os
 
 totalSize = 0
 for filename in os.listdir('/home/ventu/Downloads'):
-    totalSize = totalSize + os.path.getsize(os.path.join('/home/ventu/Downloads', filename))
+    totalSize = totalSize + os.path.getsize(os.path.join('/home/user/', filename))
 
 print(totalSize / 1024)
